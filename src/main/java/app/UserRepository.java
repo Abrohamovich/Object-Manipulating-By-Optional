5bccd95b7080cded604df55274a74620e076eb58
Mykyta Herasymov <gerasimov08nikita@gmail.com>
2024-11-23 01:20:11 +0200

    Initial commit: User and UserRepository class created

---
+++ src/main/java/app/UserRepository.java
@@ -0,0 +1,11 @@
+package app;
+
+import app.model.User;
+
+import java.util.List;
+
+public class UserRepository {
+    private List<User> users;
+
+
+}
---
+++ src/main/java/app/model/User.java
@@ -0,0 +1,25 @@
+package app.model;
+
+public class User {
+    int id;
+    String name;
+    String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
